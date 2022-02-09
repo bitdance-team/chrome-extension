b2122d774cd43c534913e40a05c6fb855975d76a
xiao_io <2238929232@qq.com>
2022-02-09 20:04:26 +0800

    类名bug

--- packages/shell-chrome/assets/js/mouse-effects/click.js
+++ packages/shell-chrome/assets/js/mouse-effects/click.js
@@ -30,7 +30,7 @@ $(function() {
             // 走起（初始化）
             function walk() {
                 // 灵魂 css
-                css(".heart{width: 10px;height: 10px;position: fixed;background: pink;transform: rotate(45deg);}.heart::after,.heart::before{position: absolute;content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;}.heart::after{top: -5px;}.heart::before{left: -5px;}");
+                css("#heart{width: 10px;height: 10px;position: fixed;background: pink;transform: rotate(45deg);}#heart::after,#heart::before{position: absolute;content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;}#heart::after{top: -5px;}#heart::before{left: -5px;}");
                 attachEvent();
                 gameloop();
             }
@@ -62,7 +62,7 @@ $(function() {
             function createHeart(event) {
                 // 创建一个 div 并指定类名为 heart
                 var d = document.createElement("div");
-                d.className = "heart";
+                d.id = "heart";
                 /**
                  * @param el: 每个 div 的标识
                  * @param x,y: 当前鼠标位置信息
