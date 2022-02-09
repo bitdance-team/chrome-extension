fceeeb078f73fe63b553fa855c60f23eb022495f
xiao_io <2238929232@qq.com>
2022-02-09 19:04:27 +0800

    整体布局和番茄钟、天气合并

--- packages/shell-chrome/assets/html/pomodoro/background.js
+++ packages/shell-chrome/assets/html/pomodoro/background.js
@@ -1,15 +1,15 @@
 // 用户首次安装插件时执行一次，后面不会再重新执行(除非用户重新安装插件)
 chrome.runtime.onInstalled.addListener(() => {
-  // 插件功能安装默认启用
-  chrome.storage.sync.set({
-    //初始化数据
-    pomoData: {
-      minutes: 24,
-      seconds: 60,
-      countdownTimer: "25:00",
-      status: "init",
-    },
-  });
+    // 插件功能安装默认启用
+    chrome.storage.sync.set({
+        //初始化数据
+        pomoData: {
+            minutes: 24,
+            seconds: 60,
+            countdownTimer: "25:00",
+            status: "init",
+        },
+    });
 });
 
 let minutes, seconds;
@@ -22,134 +22,132 @@ let array = ["minutes", "seconds", "pause", "countdownTimer", "pbutton"];
 let timer = null;
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log(`进入 assets\html\pomodoro\background.js 中的onMessage Listener`)
-  if (message.senderId !== "pomo") {
-    // 抛给下一个Listener
+    console.log(`进入 assets\html\pomodoro\background.js 中的onMessage Listener`)
+    if (message.senderId !== "pomo") {
+        // 抛给下一个Listener
+        sendResponse();
+    }
+
+    const { status, content } = message;
+
+    if (status === "start") {
+        // sendResponse({
+        //   status:message.status
+        // })
+        countdown({...content, status });
+    } else if (status === "paused") {
+        clearTimeout(timer);
+        chrome.storage.sync.get("pomoData", ({ pomoData }) => {
+            console.log(pomoData);
+            chrome.storage.sync.set({ pomoData: {...pomoData, status: "paused" } });
+        });
+    } else if (status === "reset") {
+        clearTimeout(timer);
+        chrome.storage.sync.set({
+            pomoData: {
+                minutes: 24,
+                seconds: 60,
+                countdownTimer: "25:00",
+                status: "init",
+            },
+        });
+        chrome.browserAction.setBadgeText({ text: "" });
+    } else if (status === "init") {
+        //init初始化
+        chrome.storage.sync.set({
+            pomoData: {
+                minutes: 24,
+                seconds: 60,
+                countdownTimer: "25:00",
+                status: "init",
+            },
+        });
+    } else {
+        //playend 初始化
+        chrome.storage.sync.set({
+            pomoData: {
+                minutes: 24,
+                seconds: 60,
+                countdownTimer: "25:00",
+                status: "playend",
+            },
+        });
+    }
+
+    //创建结束通知：待完成
+
+    chrome.storage.sync.set({
+        status,
+    });
+    //后台播放完成提示音乐
+    if (message.action === "play") {
+        // audio.play();
+    }
     sendResponse();
-  }
-
-  const { status, content } = message;
-
-  if (status === "start") {
-    // sendResponse({
-    //   status:message.status
-    // })
-    countdown({ ...content, status });
-  } else if (status === "paused") {
-    clearTimeout(timer);
-    chrome.storage.sync.get("pomoData", ({ pomoData }) => {
-      console.log(pomoData);
-      chrome.storage.sync.set({ pomoData: { ...pomoData, status: "paused" } });
-    });
-  } else if (status === "reset") {
-    clearTimeout(timer);
-    chrome.storage.sync.set({
-      pomoData: {
-        minutes: 24,
-        seconds: 60,
-        countdownTimer: "25:00",
-        status: "init",
-      },
-    });
-    chrome.browserAction.setBadgeText({ text: "" });
-  } else if (status === "init") {
-    //init初始化
-    chrome.storage.sync.set({
-      pomoData: {
-        minutes: 24,
-        seconds: 60,
-        countdownTimer: "25:00",
-        status: "start",
-      },
-    });
-  } else {
-    //playend 初始化
-    chrome.storage.sync.set({
-      pomoData: {
-        minutes: 24,
-        seconds: 60,
-        countdownTimer: "25:00",
-        status: "playend",
-      },
-    });
-  }
-
-  //创建结束通知：待完成
-
-  chrome.storage.sync.set({
-    status,
-  });
-  //后台播放完成提示音乐
-  if (message.action === "play") {
-    // audio.play();
-  }
-  sendResponse();
-  console.log(`离开 assets\html\pomodoro\background.js 中的onMessage Listener`)
-  return true;
+    console.log(`离开 assets\html\pomodoro\background.js 中的onMessage Listener`)
+    return true;
 });
 
 // 番茄钟倒计时功能
 function countdown({ minutes, seconds, status }) {
-  // 设置分钟和秒数
-  // let currentMins = minutes - 1;
-  seconds--;
-  let currentTimer =
-    (minutes < 10 ? "0" : "") +
-    minutes +
-    ":" +
-    (seconds < 10 ? "0" : "") +
-    seconds;
-  // countdownTimer.innerHTML = currentTimer; 拿到
-  console.log("分秒=============", minutes, seconds);
+    // 设置分钟和秒数
+    // let currentMins = minutes - 1;
+    seconds--;
+    let currentTimer =
+        (minutes < 10 ? "0" : "") +
+        minutes +
+        ":" +
+        (seconds < 10 ? "0" : "") +
+        seconds;
+    // countdownTimer.innerHTML = currentTimer; 拿到
+    console.log("分秒=============", minutes, seconds);
 
-  chrome.storage.sync.set(
-    {
-      pomoData: {
-        seconds: seconds,
-        minutes: minutes,
-        countdownTimer: currentTimer,
-        status,
-      },
-    },
-    function () {
-      if (!chrome.runtime.error) {
-        console.log("started");
-      }
-    }
-  );
-
-  //设置badge文本用来显示剩余分钟数
-  chrome.storage.sync.get("pomoData", ({ pomoData }) => {
-    chrome.browserAction.setBadgeText({ text: pomoData.minutes.toString() + ":" + pomoData.seconds.toString() });
-  });
-
-  console.log(currentTimer);
-  // count down every second, when a minute is up, countdown one minute
-  // when time reaches 0:00, reset
-  if (seconds > 0) {
-    timer = setTimeout(() => {
-      countdown({ minutes, seconds, status });
-    }, 1000);
-  } else if (minutes > 0) {
-    seconds = 60;
-    minutes--;
-    chrome.storage.sync.set(
-      {
-        pomoData: {
-          seconds: seconds,
-          minutes: minutes,
-          countdownTimer: currentTimer,
-          status,
+    chrome.storage.sync.set({
+            pomoData: {
+                seconds: seconds,
+                minutes: minutes,
+                countdownTimer: currentTimer,
+                status,
+            },
         },
-      },
-      function () {
-        if (!chrome.runtime.error) {
-          console.log("started");
+        function() {
+            if (!chrome.runtime.error) {
+                console.log("started");
+            }
         }
-      }
     );
-    countdown({ minutes, seconds, status });
-  }
+
+    //设置badge文本用来显示剩余分钟数
+    chrome.storage.sync.get("pomoData", ({ pomoData }) => {
+        chrome.browserAction.setBadgeText({ text: pomoData.minutes.toString() + ":" + pomoData.seconds.toString() });
+    });
+
+    console.log(currentTimer);
+    // count down every second, when a minute is up, countdown one minute
+    // when time reaches 0:00, reset
+    if (seconds > 0) {
+        timer = setTimeout(() => {
+            countdown({ minutes, seconds, status });
+        }, 1000);
+    } else if (minutes > 0) {
+        seconds = 60;
+        minutes--;
+        chrome.storage.sync.set({
+                pomoData: {
+                    seconds: seconds,
+                    minutes: minutes,
+                    countdownTimer: currentTimer,
+                    status,
+                },
+            },
+            function() {
+                if (!chrome.runtime.error) {
+                    console.log("started");
+                }
+            }
+        );
+        countdown({ minutes, seconds, status });
+    }
 }
 
 //设置badge文本背景颜色
---
+++ packages/shell-chrome/assets/js/popup2.js
@@ -0,0 +1,99 @@
+//天气 
+let key_w = "314285a761da4025bd4c09339dca5f0f"
+var longitude = 116.40
+var latitude = 39.90
+
+var searchWeatherfn = async function() {
+    // 用经纬度获取
+    let url_jw = longitude + ',' + latitude;
+    console.log("url_jw: " + url_jw);
+    let httpUrl = `https://geoapi.qweather.com/v2/city/lookup?location=${url_jw}&key=${key_w}`;
+    //获取城市的ID
+    // let httpUrl = `https://geoapi.qweather.com/v2/city/lookup?location=黔江区&adm=重庆&key=${key}`
+    let res1 = await fetch(httpUrl); // 不能res，会冲突
+    let result = await res1.json();
+    console.log("下面是result");
+    console.log(result);
+    document.getElementById("weatherSite").innerHTML = result.location[0].adm1;
+    let id = result.location[0].id;
+    console.log(id);
+    //根据城市id获取具体的天气
+    let httpUrl1 = `https://devapi.qweather.com/v7/weather/now?location=${id}&key=${key_w}`
+    let res2 = await fetch(httpUrl1);
+    let result1 = await res2.json();
+
+    console.log(result1)
+    console.log(result1.now);
+    //显示天气情况
+    let now = result1.now.text;
+    document.getElementById("weatherText").innerHTML = now;
+    //显示温度
+    let nowTemp = result1.now.temp
+    document.getElementById("weatherTemp").innerHTML = nowTemp + '°';
+    //显示图标
+    let svg_icon = result1.now.icon;
+    document.getElementById("svg_w").src = "./assets/image/weather/icons/" + svg_icon + ".svg";
+    //切换背景色
+    var bobyEI = document.body;
+    // var bobyEI = document.getElementById("weather")
+    svg_icon = parseInt(svg_icon);
+    if (100 <= svg_icon && svg_icon < 101) {
+        bobyEI.style = "background: url(./assets/image/weather/sun.webp) no-repeat; background-size: cover;"
+    } else if (101 <= svg_icon && svg_icon < 104) {
+        bobyEI.style = "background: url(./assets/image/weather/yun.webp) no-repeat; background-size: cover;"
+    } else if (104 <= svg_icon && svg_icon < 150) {
+        bobyEI.style = "background: url(./assets/image/weather/yin.webp) no-repeat; background-size: cover;"
+    } else if (150 <= svg_icon && svg_icon < 300) {
+        bobyEI.style = "background: url(./assets/image/weather/night.webp) no-repeat; background-size: cover;"
+    } else if (300 <= svg_icon && svg_icon < 400) {
+        bobyEI.style = "background: url(./assets/image/weather/yu.webp) no-repeat; background-size: cover;"
+    } else if (400 <= svg_icon && svg_icon < 500) {
+        bobyEI.style = "background: url(./assets/image/weather/xue.webp) no-repeat; background-size: cover;"
+    } else {
+        bobyEI.style = "background: url(./assets/image/weather/yun.webp) no-repeat; background-size: cover;"
+    }
+}
+
+function find_site() { //获取浏览器的经纬度
+    console.log("开始定位浏览器位置")
+    localStorage.removeItem('BitD_longitude')
+    localStorage.removeItem('BitD_latitude')
+    navigator.geolocation.getCurrentPosition(onSuccess); // 获取经纬度
+    function onSuccess(position) {
+        //经度
+        longitude = position.coords.longitude;
+        localStorage.setItem('BitD_longitude', longitude)
+        console.log("longitude:" + longitude)
+
+        //纬度
+        latitude = position.coords.latitude;
+        localStorage.setItem('BitD_latitude', latitude)
+        console.log("latitude:" + latitude)
+        console.log("获得浏览器经纬度而且写入localstorage end")
+    }
+}
+
+if (localStorage.getItem('BitD_longitude')) { //没有缓存
+    longitude = localStorage.getItem('BitD_longitude')
+    latitude = localStorage.getItem('BitD_latitude')
+    searchWeatherfn()
+} else { // 没有缓存的时候 或者 需要更新的时候
+    console.log("没有找到")
+    find_site()
+    searchWeatherfn()
+}
+
+document.getElementById("weatherSite").onclick = function() {
+    find_site()
+    searchWeatherfn()
+}
+
+// //天气end
+//翻译
+//默认参数
+// document.getElementById("TOtans").onclick = function () {
+//   console.log(chrome.extension.getBackgroundPage())
+//   chrome.extension.getBackgroundPage().tranBit()
+// }
+
+//翻译end
